63676a4824287dae26349af803ad29dde49cc89d
Anderson <ajr.parra@yavirac.edu.ec>
2024-11-20 14:08:30 -0500

    declaration variables

--- src/main.go
+++ src/main.go
@@ -5,12 +5,51 @@ import (
 )
 
 func main() {
+	// Hello world
 	fmt.Println("Hello, world with go")
 
-	// Variables
+	// Declaracion de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
 
-	var name string = "hello"
-	fmt.Println(name + " " + "Anderson")
+	fmt.Println(pi, pi2)
+
+	// Declaration variables entire
+	base := 12
+	var altura int = 14
+	fmt.Println(base * altura)
+
+	//Zero values => valores por defecto
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Calcular el area de un cuadrado
+
+	var ladoA int = 10
+	var ladoB int = 10
+	var area = ladoA * ladoB
+
+	fmt.Println("Area del cuadrado: ", area)
+
+	// Operaciones aritemeticas sumas, restas, multiplicaciones y divisiones
+	x := 50
+	y := 100
+
+	suma := x + y
+	fmt.Println("Suma: ", suma)
+
+	resta := x - y
+	fmt.Println("Resta: ", resta)
+
+	divisiones := x / y
+	fmt.Println("Division: ", divisiones)
+
+	multiplicaciones := x * y
+	fmt.Println("Multiplicaciones: ", multiplicaciones)
 
 	var count int = 10
 	fmt.Println("Count: ", count)
